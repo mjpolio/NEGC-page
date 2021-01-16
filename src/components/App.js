b60d93cde35c72e1c8904c5d00f4868b7e483591
Matthew Polio <mjpolio@gmail.com>
2021-01-15 20:15:56 -0800

    prelim setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import  './App.css';
+import Header from './Header';
+import Navbar from './Navbar';
+import Body from './Body';
+import About from './About';
+import Footer from './Footer';
 
 const App = () => {
-    return <div>New App</div>
+    return (
+        <div className="container">
+            <Header />
+            <BrowserRouter>
+                <div className="flex-container">
+                    <Navbar />
+                    <div className="body-item">
+                        <Switch>
+                            <Route path="/" exact component={Body}/>
+                            <Route path="/about" exact component={About}/>
+                        
+                        </Switch>
+                    </div>
+                </div>
+            </BrowserRouter>
+            <Footer />
+        </div>
+    )
 }
 
 export default App;
---
+++ src/components/Body.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Body.css';
+
+const Body = () => {
+    return (
+        <div className="body-section">
+           <h2>Norwood Evening Gardening Club</h2>
+           <p className="body-p">The Norwood Evening Garden Club brings together enthusiastic 
+               gardeners from Norwood, Walpole, Westwood and surrounding towns 
+               for the goals of:
+               <ul>
+                   <li>Learning more about horticulture and floral design</li>
+                   <li>Sharing knowledge and expertise with each other and with the public</li>
+                   <li>Making our communities more beautiful through civic projects</li>
+               </ul>
+            </p>
+            <p className="body-p">The public is cordially invited to attend any of our meetings 
+                held at the Carriage House at the First Baptist Church, 71 Bond Street 
+                in Norwood (unless noted as members-only) featuring an outstanding 
+                array of regional experts on topics in horticulture, floral design 
+                and environmental issues. Our meetings are generally held on the last 
+                Tuesday of the month from September through May.
+            </p>
+            <p className="body-p">For further information, contact membership@norwoodeveninggardenclub.com</p>
+        </div>
+    )
+}
+
+export default Body;
