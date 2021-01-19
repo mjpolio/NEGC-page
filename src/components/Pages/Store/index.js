7d78a910850a3bbdff3fe7d3d197569747624b94
Matthew Polio <mjpolio@gmail.com>
2021-01-18 18:41:12 -0800

    added font-awesome & react icons

--- src/components/Pages/Store/index.js
+++ src/components/Pages/Store/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './styles.scss';
 import { Link } from 'react-router-dom';
+import { GiLargeDress, GiGloves } from 'react-icons/gi';
+import { FaBook } from 'react-icons/fa';
+
 
 const Store = () => {
 	return (
@@ -10,10 +13,19 @@ const Store = () => {
 			</h2>
 			<div className="store-list">
 				<li className="store-item">
-					<Link to="/cookbooks">Cookbooks</Link>
+					<Link to="/cookbooks">
+					<FaBook className="store-icon" />
+					Cookbooks
+					</Link>
 				</li>
-				<li className="store-item">Aprons</li>
-				<li className="store-item">Gloves</li>
+				<li className="store-item" >
+					<GiLargeDress className="store-icon" />
+					Aprons
+				</li>
+				<li className="store-item">
+					<GiGloves className="store-icon" />
+					Gloves
+				</li>
 			</div>
 			
 		</div>
