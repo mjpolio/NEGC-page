77fd62761a92b38906a8a7df199c9e7e1494f6f1
Matthew Polio <mjpolio@gmail.com>
2021-01-21 20:21:01 -0800

    newsletter section

--- src/components/Pages/Newsletters/index.js
+++ src/components/Pages/Newsletters/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
-
 import './style.scss';
+
+import spring2017 from './src/06 - NEGC SPRING 2017 Newsletter_final2.pdf';
+import fall2016 from './src/05 - NEGC FALL 2016 Newsletter final.pdf';
+import spring2015 from './src/04 - NEGC Spring 2015 Newsletter.pdf';
+import fall2014 from './src/03- NEGC Special Travel EditionNewsletter.pdf';
+import spring2014 from './src/02 - NEGCSpring 2014Newsletter.pdf';
+import spring2013 from './src/01 - Newsletter_Feb_2013.pdf';
+
+
 
 const Newsletters = () => {
 	return (
@@ -9,6 +17,24 @@ const Newsletters = () => {
 				Newsletters
 				<div className='underline'></div>
 			</h2>
+			<div className="newsletters-item">
+				<a href={spring2017} className="newsletters-link">Spring 2017</a>
+			</div>
+			<div className="newsletters-item">
+				<a href={fall2016} className="newsletters-link">Fall 2016</a>
+			</div>
+			<div className="newsletters-item">
+				<a href={spring2015} className="newsletters-link">Spring 2015</a>
+			</div>
+			<div className="newsletters-item">
+				<a href={fall2014} className="newsletters-link">Fall 2014</a>
+			</div>
+			<div className="newsletters-item">
+				<a href={spring2014} className="newsletters-link">Spring 2014</a>
+			</div>
+			<div className="newsletters-item">
+				<a href={spring2013} className="newsletters-link">Feb 2013</a>
+			</div>
 		</div>
 	);
 };
