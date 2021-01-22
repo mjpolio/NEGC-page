94b18ed09f2d47efb3bef9a863a5445a7dd95e71
Matthew Polio <mjpolio@gmail.com>
2021-01-21 20:02:28 -0800

    news section

---
+++ src/components/Pages/News/data.js
@@ -0,0 +1,116 @@
+import img13 from './src/13 - scholarship2020.jpeg';
+import pdf13 from './src/13 - NEGC Scholarship Awarded 2020.pdf';
+import img12 from './src/12 - scholarship2018.jpeg';
+import pdf12 from './src/12 - NEGC Scholarship Awarded.pdf';
+import img11 from './src/11 - officers2018.jpg';
+import pdf11 from './src/11 - NEGC New Slate of Officers 2018-20.pdf';
+import img10 from './src/10 - flowershow.jpeg';
+import pdf10 from './src/10 -NEGC wins big at Newport.pdf';
+import img09 from './src/09 - librarydonation.jfif';
+import pdf09 from './src/09 - Walpole library donation.pdf';
+import img08 from './src/08 - morsehouse.jpeg';
+import pdf08 from './src/08 - NEGC Thanks Morse House.pdf';
+import img07 from './src/07 - Garden Therapy 1 Jan 2017.jpg';
+import img06 from './src/06 - committee meeting for 20th anniversary 1.jpg';
+import img05 from './src/05 - New slate of officers.jpeg';
+import pdf05 from './src/05 - NEGC New Slate of Officers 2016-2018.pdf';
+import img04 from './src/04 - Dale Day  Nancy Costa.jpeg';
+import pdf04 from './src/04 - NEGC Thanks Morse House.pdf';
+import img03 from './src/03 - Scholarship Award 2016.jpg';
+import pdf03 from './src/03 - NEGC Scholarship Awarded.pdf';
+import img02 from './src/02 - norwood Lib donation.jpg';
+import pdf02 from './src/02 - Norwood library donation.pdf';
+import img01 from './src/01 - NEGC Garden Therapy2.jpg';
+import pdf01 from './src/01 - Norwood Evening Garden Club Garden Therapy 2015.pdf';
+
+const imgs = [
+    {
+        id: 1,
+        image: img13,
+        info: 'Garden Club Awards Scholarship 2020',
+        pdf: pdf13,
+        date: 'July 2020'
+    },
+    {
+        id: 2,
+        image: img12,
+        info: 'Garden Club Awards Scholarship 2018',
+        pdf: pdf12,
+        date: 'July 2018'
+    },
+    {
+        id: 3,
+        image: img11,
+        info: 'New slate of Officers 2018-2020',
+        pdf: pdf11,
+        date: 'July 2018'
+    },
+    {
+        id: 4,
+        image: img10,
+        info: 'NEGC wins big at Newport Flower Show',
+        pdf: pdf10,
+        date: 'July 2018'
+    },
+    {
+        id: 5,
+        image: img09,
+        info: 'Garden Club Makes Donation to Walpole Library',
+        pdf: pdf09,
+        date: 'August 2017'
+    },
+    {
+        id: 6,
+        image: img08,
+        info: 'Norwood Evening Garden Club Thanks Morse House',
+        pdf: pdf08,
+        date: 'July 2017'
+    },
+    {
+        id: 7,
+        image: img07,
+        info: 'Norwood Evening Garden Club members provide Garden Therapy to residents of the Ellis Nursing and Rehabilitation Center several times a year.',
+    },
+    {
+        id: 8,
+        image: img06,
+        info: 'Norwood Evening Garden Club members meet to plan activities to celebrate the club\'s 20th anniversary',
+    },
+    {
+        id: 9,
+        image: img05,
+        info: 'New slate of Officers 2016-18',
+        pdf: pdf05,
+        date: 'September 2016'
+    },
+    {
+        id: 10,
+        image: img04,
+        info: 'Garden Club Thanks Morse House',
+        pdf: pdf04,
+        date: 'September 2016'
+    },
+    {
+        id: 11,
+        image: img02,
+        info: 'Garden Club Makes Donation to Norwood Library 2016',
+        pdf: pdf02,
+        date: 'July 2016'
+    },
+    {
+        id: 12,
+        image: img01,
+        info: 'Garden Therapy at Ellis Nursing & Rehabilitation Center',
+        pdf: pdf01,
+        date: 'November 2015'
+    },
+    {
+    id: 13,
+    image: img03,
+    info: 'Garden Club Awards Scholarship 2016',
+    pdf: pdf03,
+    date: 'July 2015'
+    }
+];
+
+export default imgs;
--- src/components/Pages/News/index.js
+++ src/components/Pages/News/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import './style.scss';
+import imgs from './data';
 
 const News = () => {
+
+	const renderedList = imgs.map((item) => {
+		return (
+			<div className="news-card">
+					<div className="news-image">
+						<img src={item.image} className="news-image" alt="garden" />	
+					</div>
+					<div className="news-info">
+						<a 
+							href={item.pdf} 
+							className="news-link" 
+							target="_blank"
+							rel="noreferrer noopener"
+						>
+							{item.info}
+						</a>
+						<div className="news-date">{item.date}</div>
+					</div>
+			</div>
+		)
+	});
+
 	return (
 		<div className='news'>
 			<h2 className='heading heading-primary'>
 				News
 				<div className="underline"></div>
 			</h2>	
+			{renderedList}
 		</div>
 	);
 };
